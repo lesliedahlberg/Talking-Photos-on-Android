44c4e63a3ed666a801c556d665492b87395658c7
Filip Lagerholm <filip.lagerholm@outlook.com>
2015-05-06 18:11:04 +0200

    Testing Commit From Android Studio

--- DVA217AndroidProject/app/src/main/java/com/dva217_grupp_1/dva217androidproject/MainActivity.java
+++ DVA217AndroidProject/app/src/main/java/com/dva217_grupp_1/dva217androidproject/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
     int i;
+    int j; // testing
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
